581c2c03f7a2bf8be461ef61f315e44d86d77acb
Gianella Leon <giigl@hotmail.com>
2023-03-23 18:36:42 -0500

    side bar

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,15 @@
 import { NavBar } from "../components/NavBar";
+import Sidebar from "../components/SideBar";
 
 export const Home = () => {
   return (
-    <div>
-      <NavBar />
+    <div className="max-h-screen">
+      <div style={{ height: "7.5vh" }}>
+        <NavBar />
+      </div>
+      <div className="flex" style={{ height: "92.5vh" }}>
+        <Sidebar />
+      </div>
     </div>
   );
 };
