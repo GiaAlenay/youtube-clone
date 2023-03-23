6972ac4cc0ca6cd32513bd9462acf8fc3b902643
Gianella Leon <giigl@hotmail.com>
2023-03-22 21:40:50 -0500

    navbar icons

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,12 @@
+import { AiOutlineSearch, AiOutlineClose } from "react-icons/ai";
+import { TiMicrophone } from "react-icons/ti";
+import { BsYoutube, BsCameraVideo, BsBell } from "react-icons/bs";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { IoAppsSharp } from "react-icons/io5";
+export const NavBar = () => {
+  return (
+    <div className="flex justify-between items-center px-14 h-14 bg-[#212121] opacity-95 sticky top-0 -z-50">
+      gdd
+    </div>
+  );
+};
