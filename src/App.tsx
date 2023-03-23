2cfd3b28a29d60f20677f7f59690e3f615fd192e
Gianella Leon <giigl@hotmail.com>
2023-03-22 20:21:41 -0500

    some routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Search } from "./pages/Search";
+import { Watch } from "./pages/Watch";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes></Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/watch:id" element={<Watch />} />
+      </Routes>
     </BrowserRouter>
   );
 }
